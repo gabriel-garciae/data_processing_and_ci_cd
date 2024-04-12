11268d32b8258dc64ce20a0042bfdb3f8224e6a2
gabriel-garciae <eng.gabrielgevangelista@gmail.com>
2024-04-12 11:33:44 -0300

    build: tradução comentários e criação do readme

--- src/backend.py
+++ src/backend.py
@@ -5,14 +5,14 @@ import os
 
 load_dotenv(".env")
 
-# Lê as variáveis de ambiente
+# Read environment variables
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 
-# Cria a URL de conexão com o banco de dados
+# Create the database connection URL
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
@@ -20,24 +20,24 @@ def process_excel(uploaded_file):
     try:
         df = pd.read_excel(uploaded_file)
 
-        # Verificar se há colunas extras no DataFrame
+        # Check if there are extra columns in the DataFrame
         extra_cols = set(df.columns) - set(Vendas.model_fields.keys())
         if extra_cols:
-            return False, f"Colunas extras detectadas no Excel: {', '.join(extra_cols)}"
+            return False, f"Extra columns detected in Excel: {', '.join(extra_cols)}"
 
-        # Validar cada linha com o schema escolhido
+        # Validate each line with the chosen schema
         for index, row in df.iterrows():
             try:
                 _ = Vendas(**row.to_dict())
             except Exception as e:
-                raise ValueError(f"Erro na linha {index + 2}: {e}")
+                raise ValueError(f"Line error {index + 2}: {e}")
 
         return df, True, None
 
     except ValueError as ve:
         return df, False, str(ve)
     except Exception as e:
-        return df, False, f"Erro inesperado: {str(e)}"
+        return df, False, f"Unexpected error: {str(e)}"
     
 def excel_to_sql(df):
     df.to_sql('vendas', con=DATABASE_URL, if_exists='replace', index=False)
